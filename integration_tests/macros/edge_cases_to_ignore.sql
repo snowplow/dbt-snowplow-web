c29f276acf66abfa3c9b4f833c677be8083c2fed
bill-warner <71764352+bill-warner@users.noreply.github.com>
2021-08-02 17:12:05 +0300

    Update Redshift integration test data (Close #8)

--- integration_tests/macros/edge_cases_to_ignore.sql
+++ integration_tests/macros/edge_cases_to_ignore.sql
@@ -1,11 +1,7 @@
 {# Filters out edge cases in test data set which we haven't yet solved for #}
 {% macro edge_cases_to_ignore() %}
-  {% if target.type == 'bigquery' %}
-    user_id not in (
-      'stray page ping', -- Known unsolved issue https://github.com/snowplow/data-models/issues/92
-      'NULL domain_userid' -- Case when `domain_userid` is null but `domain_sessionid` is not null. Shouldn't happen. Will solve if it arises.
-      )
-  {% else %}
-    true
-  {% endif %}
+  user_id not in (
+    'stray page ping', -- Known unsolved issue https://github.com/snowplow/data-models/issues/92
+    'NULL domain_userid' -- Case when `domain_userid` is null but `domain_sessionid` is not null. Shouldn't happen. Will solve if it arises.
+    )
 {% endmacro %}
