5f07e3d56f5969bef1963bb3f5ab4d56a94b478a
bill-warner <71764352+bill-warner@users.noreply.github.com>
2021-08-09 21:07:24 +0300

    Fix filters to determine sessions_this_run (Close #12)

--- models/base/scratch/snowplow_web_base_sessions_this_run.sql
+++ models/base/scratch/snowplow_web_base_sessions_this_run.sql
@@ -12,16 +12,25 @@
   ) 
 }}
 
-{%- set lower_limit, upper_limit, session_lookback_limit = snowplow_utils.return_base_new_event_limits(ref('snowplow_web_base_new_event_limits')) %}
+{%- set lower_limit, 
+        upper_limit,
+         _,
+        lower_limit_minus_max_session_days  = snowplow_utils.return_base_new_event_limits(ref('snowplow_web_base_new_event_limits')) %}
 
 select
   s.session_id,
   s.domain_userid,
   s.start_tstamp,
-  s.end_tstamp
+  -- end_tstamp used in next step to limit events. When backfilling, set end_tstamp to upper_limit if end_tstamp > upper_limit. 
+  -- This ensures we don't accidentally process events after upper_limit
+  case when s.end_tstamp > {{ upper_limit }} then {{ upper_limit }} else s.end_tstamp end as end_tstamp 
 
 from {{ ref('snowplow_web_base_sessions_lifecycle_manifest')}} s
 
-where 
-   (s.end_tstamp between {{ lower_limit }} and {{ upper_limit }})
-or (s.start_tstamp between {{ lower_limit }} and {{ upper_limit }})
+where
+-- General window of start_tstamps to limit table scans. Logic complicated by backfills.
+-- To be within the run, session start_tstamp must be >= lower_limit - max_session_days as we limit end_tstamp in manifest to start_tstamp + max_session_days
+s.start_tstamp >= {{ lower_limit_minus_max_session_days }}
+and s.start_tstamp <= {{ upper_limit }}
+-- Select sessions within window that either; start or finish between lower & upper limit, start and finish outside of lower and upper limits
+and not (s.start_tstamp > {{ upper_limit }} or s.end_tstamp < {{ lower_limit }})
