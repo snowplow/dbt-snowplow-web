25803fd34f1227bfef3b595aad3d9ab89ee665fa
bill-warner <71764352+bill-warner@users.noreply.github.com>
2021-08-13 11:38:22 +0100

    Remove dbt_date dependancy (Close #18)

--- models/page_views/scratch/snowplow_web_pv_engaged_time.sql
+++ models/page_views/scratch/snowplow_web_pv_engaged_time.sql
@@ -16,7 +16,7 @@ select
     -- count(distinct) counts duplicates only once
     -- adding snowplow__min_visit_length accounts for the page view event itself.
 
-  {{ var("snowplow__heartbeat", 10) }} * (count(distinct(floor({{ dbt_date.to_unixtimestamp('ev.derived_tstamp') }}/{{ var("snowplow__heartbeat", 10) }}))) - 1) + {{ var("snowplow__min_visit_length", 5) }} as engaged_time_in_s
+  {{ var("snowplow__heartbeat", 10) }} * (count(distinct(floor({{ snowplow_utils.to_unixtstamp('ev.derived_tstamp') }}/{{ var("snowplow__heartbeat", 10) }}))) - 1) + {{ var("snowplow__min_visit_length", 5) }} as engaged_time_in_s
 
 from {{ ref('snowplow_web_base_events_this_run') }} as ev
 
