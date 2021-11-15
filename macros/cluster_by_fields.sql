b6b2135fd13dee03ecbf9b2740e153d43ccfae51
bill-warner <71764352+bill-warner@users.noreply.github.com>
2021-11-15 10:09:11 +0000

    Fix cluster_by_fields macros to allow overriding (Close #35)

--- macros/cluster_by_fields.sql
+++ macros/cluster_by_fields.sql
@@ -1,11 +1,23 @@
 {% macro cluster_by_fields_sessions_lifecycle() %}
 
+  {{ return(adapter.dispatch('cluster_by_fields_sessions_lifecycle', snowplow_web._get_namespaces())()) }}
+
+{% endmacro %}
+
+{% macro default__cluster_by_fields_sessions_lifecycle() %}
+
   {{ return(snowplow_utils.get_cluster_by(bigquery_cols=["session_id"], snowflake_cols=["to_date(start_tstamp)"])) }}
 
 {% endmacro %}
 
 
 {% macro cluster_by_fields_page_views() %}
+
+  {{ return(adapter.dispatch('cluster_by_fields_page_views', snowplow_web._get_namespaces())()) }}
+
+{% endmacro %}
+
+{% macro default__cluster_by_fields_page_views() %}
 
   {{ return(snowplow_utils.get_cluster_by(bigquery_cols=["domain_userid","domain_sessionid"], snowflake_cols=["to_date(start_tstamp)"])) }}
 
@@ -14,12 +26,24 @@
 
 {% macro cluster_by_fields_sessions() %}
 
+  {{ return(adapter.dispatch('cluster_by_fields_sessions', snowplow_web._get_namespaces())()) }}
+
+{% endmacro %}
+
+{% macro default__cluster_by_fields_sessions() %}
+
   {{ return(snowplow_utils.get_cluster_by(bigquery_cols=["domain_userid"], snowflake_cols=["to_date(start_tstamp)"])) }}
 
 {% endmacro %}
 
 
 {% macro cluster_by_fields_users() %}
+
+  {{ return(adapter.dispatch('cluster_by_fields_users', snowplow_web._get_namespaces())()) }}
+
+{% endmacro %}
+
+{% macro default__cluster_by_fields_users() %}
 
   {{ return(snowplow_utils.get_cluster_by(bigquery_cols=["user_id","domain_userid"], snowflake_cols=["to_date(start_tstamp)"])) }}
 
