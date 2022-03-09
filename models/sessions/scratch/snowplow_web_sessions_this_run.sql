7f95a8038b22326893e022910765e603f6c604a0
emielver <emiel@snowplowanalytics.com>
2022-03-09 11:04:10 +0000

    Fix user stitching bug if `domain_userid` is smaller than `user_id` (Close #66)

--- models/sessions/scratch/snowplow_web_sessions_this_run.sql
+++ models/sessions/scratch/snowplow_web_sessions_this_run.sql
@@ -30,9 +30,9 @@ select
 
   {% if var('snowplow__session_stitching') %}
     -- updated with mapping as part of post hook on derived sessions table
-    a.domain_userid as stitched_user_id, 
+    cast(a.domain_userid as {{ snowplow_utils.type_string(255) }}) as stitched_user_id,
   {% else %}
-    cast(null as {{ dbt_utils.type_string() }}) as stitched_user_id,
+    cast(null as {{ snowplow_utils.type_string(255) }}) as stitched_user_id,
   {% endif %}
   
   a.network_userid,
