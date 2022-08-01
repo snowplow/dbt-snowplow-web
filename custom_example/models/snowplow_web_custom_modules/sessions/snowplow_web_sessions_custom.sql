0fe6429af40e5f9b1c24e6f9db88928592ccbafe
georgewoodhead <50772749+georgewoodhead@users.noreply.github.com>
2022-08-01 13:15:56 +0100

    Add custom examples for all databases (Close #87)

--- custom_example/models/snowplow_web_custom_modules/sessions/snowplow_web_sessions_custom.sql
+++ custom_example/models/snowplow_web_custom_modules/sessions/snowplow_web_sessions_custom.sql
@@ -4,13 +4,22 @@
     unique_key='domain_sessionid',
     upsert_date_key='start_tstamp',
     sort='start_tstamp',
-    dist='domain_sessionid'
+    dist='domain_sessionid',
+    partition_by = snowplow_utils.get_partition_by(bigquery_partition_by = {
+      "field": "start_tstamp",
+      "data_type": "timestamp"
+    }, databricks_partition_by='start_tstamp_date'),
+    cluster_by=snowplow_web.web_cluster_by_fields_sessions(),
+    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt'))
   ) 
 }}
 
 
 select 
   s.*,
+  {% if target.type in ['databricks', 'spark'] -%}
+  , DATE(start_tstamp) as start_tstamp_date
+  {%- endif %}
   c.is_session_w_intent,
   c.is_session_w_conversion
 
--- custom_example/models/snowplow_web_custom_modules/users/snowplow_web_users_custom.sql
+++ custom_example/models/snowplow_web_custom_modules/users/snowplow_web_users_custom.sql
@@ -4,15 +4,23 @@
     unique_key='user_primary_key',
     upsert_date_key='start_tstamp',
     sort='start_tstamp',
-    dist='user_primary_key'
+    dist='user_primary_key',
+    partition_by = snowplow_utils.get_partition_by(bigquery_partition_by = {
+      "field": "start_tstamp",
+      "data_type": "timestamp"
+    }, databricks_partition_by='start_tstamp_date'),
+    cluster_by=snowplow_web.web_cluster_by_fields_users(),
+    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt'))
   ) 
 }}
 
 
-select 
-  CONCAT(u.domain_userid, "-"  s.user_ipaddress) as user_primary_key,
-  u.*,
-
+select distinct
+  {{ dbt_utils.concat(["u.domain_userid", "'-'", "s.user_ipaddress"]) }} as user_primary_key,
+  u.*
+  {% if target.type in ['databricks', 'spark'] -%}
+  , DATE(start_tstamp) as start_tstamp_date
+  {%- endif %}
 
 from {{ ref('snowplow_web_users_this_run') }} u -- join sessions_this_run to sessions_conversion_this_run to produce complete sessions table
 left join {{ ref('snowplow_web_sessions')}} s on u.domain_userid = s.domain_userid
