3f4a74994b4d7fdf7f0b4d0cd12c3fe95934e1ba
Ryan Hill <r.l.hill128@gmail.com>
2022-09-27 13:40:35 +0100

    Add databricks to stitch user macro (Close #93)

--- macros/stitch_user_identifiers.sql
+++ macros/stitch_user_identifiers.sql
@@ -8,6 +8,15 @@
     from {{ user_mapping_relation }} as um
     where s.domain_userid = um.domain_userid;
 
+  {% elif enabled and target.type in ['databricks', 'spark']  | as_bool() %}
+
+    -- Update sessions table with mapping
+    merge into {{ relation }} as s
+    using {{ user_mapping_relation }} as um
+    on s.domain_userid = um.domain_userid
+    when matched then 
+      update set s.stitched_user_id = um.user_id;
+
   {% endif %}
 
 {% endmacro %}
