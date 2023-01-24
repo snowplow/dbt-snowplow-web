d4635baa04a6cbad5b2e16a91e12ca23821ea2d2
emielver <emiel@snowplowanalytics.com>
2023-01-24 17:41:10 +0000

    Fix bug where tests fail due to NULL `domain_userid` (Close #152)

--- integration_tests/models/source/bigquery/snowplow_web_events_stg.sql
+++ integration_tests/models/source/bigquery/snowplow_web_events_stg.sql
@@ -12,10 +12,8 @@ from {{ ref('snowplow_web_events') }}
 select
   * except(contexts_com_snowplowanalytics_snowplow_web_page_1_0_0),
   array(
-    select as struct JSON_EXTRACT_scalar(json_array,'$.id') as id 
+    select as struct JSON_EXTRACT_scalar(json_array,'$.id') as id
     from unnest(contexts_com_snowplowanalytics_snowplow_web_page_1_0_0) as json_array
     ) as contexts_com_snowplowanalytics_snowplow_web_page_1_0_0
 
 from prep
-
-where {{ edge_cases_to_ignore() }} --filter out any edge cases we havent yet solved for but are included in the test dataset.
--- integration_tests/models/source/snowflake/snowplow_web_events_stg.sql
+++ integration_tests/models/source/snowflake/snowplow_web_events_stg.sql
@@ -12,5 +12,3 @@ select
   *
 
 from prep
-
-where {{ edge_cases_to_ignore() }} --filter out any edge cases we havent yet solved for but are included in the test dataset.
