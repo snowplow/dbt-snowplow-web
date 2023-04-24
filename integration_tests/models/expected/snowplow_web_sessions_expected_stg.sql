65c52912a1ea863b8f3b484198f561cf14d4777a
Ryan Hill <r.l.hill128@gmail.com>
2023-04-24 13:09:11 +0100

    v1 conversion columns

--- integration_tests/models/expected/snowplow_web_sessions_expected_stg.sql
+++ integration_tests/models/expected/snowplow_web_sessions_expected_stg.sql
@@ -1,4 +1,127 @@
 
-select * 
+SELECT
+    APP_ID,domain_sessionid
+    ,domain_sessionidx
+    ,start_tstamp
+    ,end_tstamp
+    ,user_id
+    ,domain_userid
+    ,stitched_user_id
+    ,network_userid
+    ,page_views
+    ,engaged_time_in_s
+    ,absolute_time_in_s
+    ,first_page_title
+    ,first_page_url
+    ,first_page_urlscheme
+    ,first_page_urlhost
+    ,first_page_urlpath
+    ,first_page_urlquery
+    ,first_page_urlfragment
+    ,last_page_title
+    ,last_page_url
+    ,last_page_urlscheme
+    ,last_page_urlhost
+    ,last_page_urlpath
+    ,last_page_urlquery
+    ,last_page_urlfragment
+    ,referrer
+    ,refr_urlscheme
+    ,refr_urlhost
+    ,refr_urlpath
+    ,refr_urlquery
+    ,refr_urlfragment
+    ,refr_medium
+    ,refr_source
+    ,refr_term
+    ,mkt_medium
+    ,mkt_source
+    ,mkt_term
+    ,mkt_content
+    ,mkt_campaign
+    ,mkt_clickid
+    ,mkt_network
+    ,geo_country
+    ,geo_region
+    ,geo_region_name
+    ,geo_city
+    ,geo_zipcode
+    ,geo_latitude
+    ,geo_longitude
+    ,geo_timezone
+    ,user_ipaddress
+    ,useragent
+    ,br_renderengine
+    ,br_lang
+    ,os_timezone
+    ,category
+    ,primary_impact
+    ,reason
+    ,spider_or_robot
+    ,useragent_family
+    ,useragent_major
+    ,useragent_minor
+    ,useragent_patch
+    ,useragent_version
+    ,os_family
+    ,os_major
+    ,os_minor
+    ,os_patch
+    ,os_patch_minor
+    ,os_version
+    ,device_family
+    ,device_class
+    ,agent_class
+    ,agent_name
+    ,agent_name_version
+    ,agent_name_version_major
+    ,agent_version
+    ,agent_version_major
+    ,device_brand
+    ,device_name
+    ,device_version
+    ,layout_engine_class
+    ,layout_engine_name
+    ,layout_engine_name_version
+    ,layout_engine_name_version_major
+    ,layout_engine_version
+    ,layout_engine_version_major
+    ,operating_system_class
+    ,operating_system_name
+    ,operating_system_name_version
+    ,operating_system_version
+{% if target.type in ['snowflake'] %}
+    ,cv_view_page_volume
+    ,AS_ARRAY(parse_json(cv_view_page_events)) as cv_view_page_events
+    ,AS_ARRAY(parse_json(cv_view_page_values)) as cv_view_page_values
+    ,cv_view_page_total
+    ,cv_view_page_first_conversion
+    ,cv_view_page_converted
+{% elif target.type in ['bigquery'] %}
+    ,cv_view_page_volume
+    {# BQ can't compare array columns #}
+    ,TO_JSON_STRING(ARRAY(SELECT replace(x, '"', '') FROM UNNEST(JSON_EXTRACT_ARRAY(cv_view_page_events,'$')) as x)) as cv_view_page_events
+    ,TO_JSON_STRING(ARRAY(SELECT CAST(x AS FLOAT64) FROM UNNEST(JSON_EXTRACT_ARRAY(cv_view_page_values,'$')) as x)) as cv_view_page_values
+    ,cv_view_page_total
+    ,cv_view_page_first_conversion
+    ,cv_view_page_converted
+{% elif target.type in ['spark', 'databricks'] %}
+    ,cv_view_page_volume
+    {# thank you chatGPT #}
+    ,filter(transform(split(regexp_replace(substring(cv_view_page_events, 3, length(cv_view_page_events)-3), '\\"+', ''), ','), x -> CAST(trim(x) AS string)), x -> x is not null and x != '')  as cv_view_page_events
+    ,filter(transform(split(regexp_replace(substring(cv_view_page_values, 3, length(cv_view_page_values)-3), '\\"+', ''), ','), x -> CAST(trim(x) AS double)), x -> x is not null)  as cv_view_page_values
+    ,cv_view_page_total
+    ,cv_view_page_first_conversion
+    ,cv_view_page_converted
+{% elif target.type in ['postgres'] %}
+    ,cv_view_page_volume
+    ,string_to_array(regexp_replace(cv_view_page_events, '[\[\]\"]', '', 'g'),',') as cv_view_page_events
+    ,string_to_array(regexp_replace(cv_view_page_values, '[\[\]\"]', '', 'g'),',')::numeric[] as cv_view_page_values
+    ,cv_view_page_total
+    ,cv_view_page_first_conversion
+    ,cv_view_page_converted
+{% endif %}
 
-from {{ ref('snowplow_web_sessions_expected') }}
+
+
+FROM {{ ref('snowplow_web_sessions_expected') }}
---
+++ macros/get_conversion_columns.sql
@@ -0,0 +1,144 @@
+{% macro get_conversion_columns(conv_object = {}, names_only = false) %}
+  {{ return(adapter.dispatch('get_conversion_columns', 'snowplow_web')(conv_object, names_only)) }}
+{% endmacro %}
+
+{% macro default__get_conversion_columns(conv_object, names_only = false) %}
+{% if execute %}
+    {% do exceptions.raise_compiler_error('Macro get_field only supports Bigquery, Snowflake, Spark, Databricks, Postgres, and Redshift, it is not supported for ' ~ target.type) %}
+{% endif %}
+{% endmacro %}
+
+{% macro snowflake__get_conversion_columns(conv_object, names_only = false) %}
+  {%- if not names_only %}
+    ,COUNT(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE null END) AS cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,ARRAYAGG(CASE WHEN {{ conv_object['condition'] }} THEN event_id ELSE null END) WITHIN GROUP (ORDER BY derived_tstamp, dvce_created_tstamp, event_id) AS cv_{{ conv_object['name'] }}_events
+    {%- endif -%}
+    {%- if conv_object.get('value', none) %}
+    ,ARRAYAGG(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }},{{ conv_object.get('default_value', 0) }})  ELSE null END) WITHIN GROUP (ORDER BY derived_tstamp, dvce_created_tstamp, event_id) AS cv_{{ conv_object['name'] }}_values
+    ,SUM(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }}, {{ conv_object.get('default_value', 0) }}) ELSE 0 END) AS cv_{{ conv_object['name'] }}_total
+    {%- endif %}
+    ,MIN(CASE WHEN {{ conv_object['condition'] }} THEN derived_tstamp ELSE null END) AS cv_{{ conv_object['name'] }}_first_conversion
+    ,CAST(MAX(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE 0 END) AS {{ type_boolean() }}) AS cv_{{ conv_object['name'] }}_converted
+  {%- else -%}
+    ,cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,cv_{{ conv_object['name'] }}_events
+    {%- endif %}
+    {%- if conv_object.get('value', none) %}
+    ,cv_{{ conv_object['name'] }}_values
+    ,cv_{{ conv_object['name'] }}_total
+    {%- endif %}
+    ,cv_{{ conv_object['name'] }}_first_conversion
+    ,cv_{{ conv_object['name'] }}_converted
+  {%- endif %}
+{% endmacro %}
+
+
+{% macro bigquery__get_conversion_columns(conv_object, names_only = false) %}
+  {%- if not names_only %}
+    ,COUNT(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE null END) AS cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,ARRAY_AGG(CASE WHEN {{ conv_object['condition'] }} THEN event_id ELSE null END IGNORE NULLS ORDER BY derived_tstamp, dvce_created_tstamp, event_id) AS cv_{{ conv_object['name'] }}_events
+    {%- endif -%}
+    {%- if conv_object.get('value', none) %}
+    ,ARRAY_AGG(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }},{{ conv_object.get('default_value', 0) }})  ELSE null END IGNORE NULLS ORDER BY derived_tstamp, dvce_created_tstamp, event_id) AS cv_{{ conv_object['name'] }}_values
+    ,SUM(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }}, {{ conv_object.get('default_value', 0) }})  ELSE 0 END) AS cv_{{ conv_object['name'] }}_total
+    {%- endif -%}
+    ,MIN(CASE WHEN {{ conv_object['condition'] }} THEN derived_tstamp ELSE null END) AS cv_{{ conv_object['name'] }}_first_conversion
+    ,CAST(MAX(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE 0 END) AS {{ type_boolean() }}) AS cv_{{ conv_object['name'] }}_converted
+  {%- else -%}
+    ,cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,cv_{{ conv_object['name'] }}_events
+    {%- endif %}
+    {%- if conv_object.get('value', none) %}
+    ,cv_{{ conv_object['name'] }}_values
+    ,cv_{{ conv_object['name'] }}_total
+    {%- endif %}
+    ,cv_{{ conv_object['name'] }}_first_conversion
+    ,cv_{{ conv_object['name'] }}_converted
+  {%- endif %}
+{% endmacro %}
+
+
+{% macro spark__get_conversion_columns(conv_object, names_only = false) %}
+  {%- if not names_only %}
+    ,COUNT(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE null END) AS cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    {# make an struct of the thing we want to put in an array, then the things we want to order by, collect THOSE into an array, filter out where the thing we want is null, sort those based on the other columns, then select just the thing we care about #}
+    ,transform(array_sort(FILTER(collect_list(struct(CASE WHEN {{ conv_object['condition'] }} THEN event_id ELSE null END, derived_tstamp, dvce_created_tstamp, event_id)), x -> x['col1'] is not null), (left, right) -> CASE WHEN left['derived_tstamp']  < right['derived_tstamp'] THEN -1 WHEN left['derived_tstamp']  > right['derived_tstamp'] THEN 1 WHEN left['dvce_created_tstamp']  < right['dvce_created_tstamp'] THEN -1 WHEN left['dvce_created_tstamp']  > right['dvce_created_tstamp'] THEN 1 WHEN left['event_id']  < right['event_id'] THEN -1 WHEN left['event_id']  > right['event_id'] THEN 1 ELSE 0 END), x -> x['col1'])  AS cv_{{ conv_object['name'] }}_events
+    {%- endif -%}
+    {%- if conv_object.get('value', none) %}
+    {# make an struct of the thing we want to put in an array, then the things we want to order by, collect THOSE into an array, filter out where the thing we want is null, sort those based on the other columns, then select just the thing we care about #}
+    ,transform(array_sort(FILTER(collect_list(struct(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }},{{ conv_object.get('default_value', 0) }})  ELSE null END, derived_tstamp, dvce_created_tstamp, event_id)), x -> x['col1'] is not null), (left, right) -> CASE WHEN left['derived_tstamp']  < right['derived_tstamp'] THEN -1 WHEN left['derived_tstamp']  > right['derived_tstamp'] THEN 1 WHEN left['dvce_created_tstamp']  < right['dvce_created_tstamp'] THEN -1 WHEN left['dvce_created_tstamp']  > right['dvce_created_tstamp'] THEN 1 WHEN left['event_id']  < right['event_id'] THEN -1 WHEN left['event_id']  > right['event_id'] THEN 1 ELSE 0 END), x -> x['col1'])  AS cv_{{ conv_object['name'] }}_values
+    ,SUM(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }}, {{ conv_object.get('default_value', 0) }})  ELSE 0 END) AS cv_{{ conv_object['name'] }}_total
+    {%- endif -%}
+    ,MIN(CASE WHEN {{ conv_object['condition'] }} THEN derived_tstamp ELSE null END) AS cv_{{ conv_object['name'] }}_first_conversion
+    ,CAST(MAX(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE 0 END) AS {{ type_boolean() }}) AS cv_{{ conv_object['name'] }}_converted
+  {%- else -%}
+    ,cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,cv_{{ conv_object['name'] }}_events
+    {%- endif %}
+    {%- if conv_object.get('value', none) %}
+    ,cv_{{ conv_object['name'] }}_values
+    ,cv_{{ conv_object['name'] }}_total
+    {%- endif %}
+    ,cv_{{ conv_object['name'] }}_first_conversion
+    ,cv_{{ conv_object['name'] }}_converted
+  {%- endif %}
+{% endmacro %}
+
+{% macro postgres__get_conversion_columns(conv_object = {}, names_only = false) %}
+  {%- if not names_only %}
+    ,COUNT(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE null END) AS cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,ARRAY_REMOVE(ARRAY_AGG(CASE WHEN {{ conv_object['condition'] }} THEN event_id ELSE null END ORDER BY derived_tstamp, dvce_created_tstamp, event_id), null) AS cv_{{ conv_object['name'] }}_events
+    {%- endif -%}
+    {%- if conv_object.get('value', none) %}
+    ,ARRAY_REMOVE(ARRAY_AGG(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }},{{ conv_object.get('default_value', 0) }})  ELSE null END ORDER BY derived_tstamp, dvce_created_tstamp, event_id), null) AS cv_{{ conv_object['name'] }}_values
+    ,SUM(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }}, {{ conv_object.get('default_value', 0) }})  ELSE 0 END) AS cv_{{ conv_object['name'] }}_total
+    {%- endif -%}
+    ,MIN(CASE WHEN {{ conv_object['condition'] }} THEN derived_tstamp ELSE null END) AS cv_{{ conv_object['name'] }}_first_conversion
+    ,CAST(MAX(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE 0 END) AS {{ type_boolean() }}) AS cv_{{ conv_object['name'] }}_converted
+  {%- else -%}
+    ,cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,cv_{{ conv_object['name'] }}_events
+    {%- endif %}
+    {%- if conv_object.get('value', none) %}
+    ,cv_{{ conv_object['name'] }}_values
+    ,cv_{{ conv_object['name'] }}_total
+    {%- endif %}
+    ,cv_{{ conv_object['name'] }}_first_conversion
+    ,cv_{{ conv_object['name'] }}_converted
+  {%- endif %}
+{% endmacro %}
+
+{% macro redshift__get_conversion_columns(conv_object, names_only = false) %}
+  {%- if not names_only %}
+    ,COUNT(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE null END) AS cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,SPLIT_TO_ARRAY(LISTAGG(CASE WHEN {{ conv_object['condition'] }} THEN event_id ELSE null END, ',') WITHIN GROUP (ORDER BY derived_tstamp, dvce_created_tstamp, event_id), ',') AS cv_{{ conv_object['name'] }}_events
+    {%- endif -%}
+    {%- if conv_object.get('value', none) %}
+    {# Want to try and use a symbol that is unlikely to be in the values due to redshift not having a single array_agg function, hence ~ not , #}
+    ,SPLIT_TO_ARRAY(LISTAGG(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }},{{ conv_object.get('default_value', 0) }})  ELSE null END, '~') WITHIN GROUP (ORDER BY derived_tstamp, dvce_created_tstamp, event_id), '~') AS cv_{{ conv_object['name'] }}_values
+    ,SUM(CASE WHEN {{ conv_object['condition'] }} THEN coalesce({{ conv_object['value'] }}, {{ conv_object.get('default_value', 0) }})  ELSE 0 END) AS cv_{{ conv_object['name'] }}_total
+    {%- endif -%}
+    ,MIN(CASE WHEN {{ conv_object['condition'] }} THEN derived_tstamp ELSE null END) AS cv_{{ conv_object['name'] }}_first_conversion
+    ,CAST(MAX(CASE WHEN {{ conv_object['condition'] }} THEN 1 ELSE 0 END) AS {{ type_boolean() }}) AS cv_{{ conv_object['name'] }}_converted
+  {%- else -%}
+    ,cv_{{ conv_object['name'] }}_volume
+    {%- if conv_object.get('list_events', false) %}
+    ,cv_{{ conv_object['name'] }}_events
+    {%- endif %}
+    {%- if conv_object.get('value', none) %}
+    ,cv_{{ conv_object['name'] }}_values
+    ,cv_{{ conv_object['name'] }}_total
+    {%- endif %}
+    ,cv_{{ conv_object['name'] }}_first_conversion
+    ,cv_{{ conv_object['name'] }}_converted
+  {%- endif %}
+{% endmacro %}
